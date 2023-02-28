23481401a0ddb97a2ae338b8455a70a905d42379
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-02-28 12:26:22 -0600

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,19 @@
 class UsersController < ApplicationController
-
+skip_before_action :authorize, only: :create
+    
     def create
-        user = User.create(user_params)
-        if user.valid?
+        user = User.create!(user_params)
             session[:user_id] = user.id
             render json: user, status: :created
-        else
-            render json: {error: user.errors.full_messages}, status: :unprocessable_entity
     end
 
     def show
-        user = User.find(session[:user_id])
-        if user&.authorization(params[:password])
-            session[:user_id] = user.id
-            render json: user, status: :created
-        else
-            render json: {error: "Invalid username or password"}, status: :unauthorized 
+        render json: @current_user
     end
 
     private
 
     def user_params
-        params.permit(:username, :image_url, :bio, :password)
+        params.permit(:username, :image_url, :bio, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
     has_many :recipes
+    has_secure_password
 
     validates :username, presence: true
     validates :username, uniqueness: true
